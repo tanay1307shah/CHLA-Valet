c92cabc4408e808c874836dc3a77c6c3692f06e2
Tanay Shah <tanayssh@usc.edu>
2019-02-14 16:56:58 -0800

    Baisc setup

---
+++ src/main/java/com/example/chla_server/CHLAserver/ChlaServerApplication.java
@@ -0,0 +1,14 @@
+package com.example.chla_server.CHLAserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChlaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChlaServerApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/example/chla_server/CHLAserver/ChlaServerApplicationTests.java
@@ -0,0 +1,17 @@
+package com.example.chla_server.CHLAserver;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ChlaServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
