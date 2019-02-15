c4a4c5e13c7e0e4e5541be5c4c1660d449174c26
Ali Hashemi <alihashe@usc.edu>
2019-02-14 17:07:54 -0800

    Added skeleton code for APIManager class

---
+++ CHLAValet/CHLAValet/Models/APIManager.swift
@@ -0,0 +1,18 @@
+//
+//  APIManager.swift
+//  CHLAValet
+//
+//  Created by Ali Hashemi on 2/14/19.
+//  Copyright © 2019 CSCI401. All rights reserved.
+//
+
+import Foundation
+
+class APIManager {
+    
+    let baseURL = Constants.someAPIURL
+    
+    func someAPICall() -> Void {
+        
+    }
+}
