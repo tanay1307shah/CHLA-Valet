75972ac3f0ca4d59b290e179664594db962de7c8
Tanay Shah <tanayssh@usc.edu>
2019-02-20 14:35:31 -0800

    System Update V 0.10

--- src/main/java/com/example/chla_server/CHLAserver/repositories/CarRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.chla_server.CHLAserver.repositories;
-
-import com.example.chla_server.CHLAserver.model.Car;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CarRepository extends CrudRepository<Car,Long> {
-}
