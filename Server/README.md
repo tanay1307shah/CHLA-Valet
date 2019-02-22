19e220adc8e9cc101798417b36291df933a7d0be
Tanay Shah <tanayssh@usc.edu>
2019-02-21 19:48:17 -0800

    Added Maven proj

--- Server/README.md
+++
@@ -1,1 +0,0 @@
-# CHLA-Valet
--- Server/src/main/java/com/example/CHLAserver/Repositories/RequestRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.CHLAserver.Repositories;
-
-import com.example.CHLAserver.Model.Requests;
-import org.springframework.data.repository.CrudRepository;
-
-public interface RequestRepository extends CrudRepository<Requests,Long> {
-}
