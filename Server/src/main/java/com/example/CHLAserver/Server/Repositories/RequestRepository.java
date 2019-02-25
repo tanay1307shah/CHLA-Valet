e4c057cdaf3e28ad353ea2004cdf8786a9801f78
Ali Hashemi <alihashe@usc.edu>
2019-02-25 14:03:38 -0800

    Deleted server file from app branch

--- Server/src/main/java/com/example/CHLAserver/Server/Repositories/RequestRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.CHLAserver.Server.Repositories;
-
-import com.example.CHLAserver.Server.Model.Requests;
-import com.example.CHLAserver.Server.Model.Requests;
-import org.springframework.data.repository.CrudRepository;
-
-public interface RequestRepository extends CrudRepository<Requests,Long> {
-}
